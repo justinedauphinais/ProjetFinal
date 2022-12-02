0fc87411d973030e8974e6912a73da4e28faaf04
justinedauphinais <justinedauphinaisti@gmail.com>
2022-12-02 06:43:59 -0500

    + Ressources

--- ProjetFinal/ProjetFinal.cpp
+++ ProjetFinal/ProjetFinal.cpp
@@ -3,6 +3,6 @@
 
 using namespace sf;	//en mettant ce namespace, ça évite des sf::
 int main() {
-	game flappy(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird");
+	game flappy(SCREEN_WIDTH, SCREEN_HEIGHT, "The Catacombs of Yharnam");
 	return EXIT_SUCCESS;
 }
